22712b94b66695ac90a947caf7aa593e197d945f
jul <jules.westersang@gmail.com>
2018-02-05 11:42:53 +0100

    fullscreen

--- Game.rb
+++ Game.rb
@@ -1,11 +1,13 @@
 class Game < Gosu::Window
 
   def initialize
+
     @WIDTH = 1440
     @HEIGHT = 920
     @NOM = "le nom du jeu"
 
-    super @WIDTH, @HEIGHT
+
+    super @WIDTH, @HEIGHT, options = {:fullscreen => true}
 
     self.caption = @NOM
 
